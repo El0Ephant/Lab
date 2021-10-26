1f5c11e1355b969acef053826f808ea4c93d050c
Михаил Карякин <mkariakin@sfedu.ru>
2021-10-26 17:17:26 +0300

    Add function LaterInYear

--- main.cpp
+++ main.cpp
@@ -10,6 +10,24 @@ bool isLeapYear(int year) {
 	}
 }
 
+int LaterInYear(int d1, int m1, int d2, int m2) {
+	if (d1 > d2) {
+		return 1;
+	}
+	else if (d1 < d2) {
+		return 2;
+	}
+	else {
+		if (m1 > m2) {
+			return 1;
+		}
+		else if (m1 < m2) {
+			return 2;
+		}
+		else return 0;
+	}
+}
+
 int main() {
 	assert(!isLeapYear(100));
 	assert(isLeapYear(400));

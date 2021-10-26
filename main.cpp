925853acb5db4dddc7cfaafacfd79d9306d57673
Михаил Карякин <mkariakin@sfedu.ru>
2021-10-26 17:05:42 +0300

    Add function isLeapYear

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
-int main() {
+#include <assert.h>
+using namespace std;
 
+bool isLeapYear(int year) {
+	if (year % 100 != 0) {
+		return year % 4 == 0;
+	}
+	else {
+		return year % 16 == 0;
+	}
+}
+
+int main() {
+	assert(!isLeapYear(100));
+	assert(isLeapYear(400));
+	assert(isLeapYear(404));
 }
